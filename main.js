1270f86c5a74a0015944eb3e3904842046d05afe
Elv-Ros22 <elviamanrique00@gmail.com>
2025-04-06 20:30:32 -0500

    able to display images when searching

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ function sendApiRequest() {
   console.log(userInput);
 
   var giphyApiKey = "cZV02Bhn2mkpZQnxZ09gKSsloNwgkJFT";
-  var giphyApiURL =
-    "https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=g&api_key=${giphyApiKey}";
+  var giphyApiURL = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=g&api_key=${giphyApiKey}`;
 
   fetch(giphyApiURL)
     .then(function (data) {
       return data.json();
     })
-    .then(function (json) {});
+    .then(function (json) {
+      console.log(json.data[0].images.fixed_height.url);
+      var imgPath = json.data[0].images.fixed_height.url;
+      var img = document.createElement("img");
+      img.setAttribute("src", imgPath);
+      document.body.appendChild(img);
+    });
 }
